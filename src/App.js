c843eef4b2bc582e06f1a3b50d8ea9924a1f608e
pkcnguyen@gmail.com <pkcnguyen@gmail.com>
2018-11-22 15:35:59 +0700

    add bg image

--- src/App.js
+++ src/App.js
@@ -8,18 +8,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <section className="title">
+            <span>lovedays</span>
+          </section>
           <section className="lovedays-wrapper">
             <Lovedays />
           </section>
           <section className="profile-wrapper">
             <section className="_theguy">
-              <img src="#" alt="" className="guy_pic" />
-              <div clasName="guy_name">Name</div>
+              <img src="theguy.jpg" alt="" className="guy_pic" />
+              <div clasName="guy_name">Chấn Nguyên</div>
             </section>
-            <Icon type="heart" theme="filled" />
+            <section className="heart">
+              <Icon className="_heart" type="heart" theme="filled" />
+            </section>
             <section className="_thegirl">
-              <img src="#" alt="" className="girl_pic" />
-              <div className="girl_name">name</div>
+              <img src="thegirl.png" alt="" className="girl_pic" />
+              <div className="girl_name">Bảo Trân</div>
             </section>
           </section>
         </header>
